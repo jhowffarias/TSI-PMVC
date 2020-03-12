1660dfcca1998dc9c055070b87ca981d7e1525d3
jhowffarias <49926322+jhowffarias@users.noreply.github.com>
2020-03-11 22:19:05 -0300

    Aula - 5
    Aula 5 - Exercicio sera na proxima aula

---
+++ app/Clientes.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Clientes extends Model
+{
+    protected $fillable = [
+        'id',
+        'nome',
+        'email',
+        'endereco',
+        'telefone'
+    ];
+    protected $table='Clientes';
+
+    public function vendas(){
+        return $this->hasMany(Vendas::class,'cliente_id');
+    }
+}
--- database/migrations/2020_03_11_201014_cria_tabela_clientes.php
+++ database/migrations/2020_03_11_201014_cria_tabela_clientes.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CriarTabelaProdutos extends Migration
+class CriaTabelaClientes extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,12 @@ class CriarTabelaProdutos extends Migration
      */
     public function up()
     {
-        Schema::create('Produtos', function (Blueprint $table) {
+        Schema::create('Clientes', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('nome');
-            $table->text('descricao');
-            $table->double('preco', 10, 2);
+            $table->string('email');
+            $table->string('endereco');
+            $table->bigInteger('telefone');
             $table->timestamps();
         });
     }
@@ -29,6 +30,6 @@ class CriarTabelaProdutos extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('Produtos');
+        Schema::dropIfExists('Clientes');
     }
 }
